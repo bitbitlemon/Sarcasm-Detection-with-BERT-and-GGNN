6fc81eca21370ec4edd6d47c3785cae5f1b9941e
bitlemon <136308066+bitbitlemon@users.noreply.github.com>
2025-02-22 10:32:21 +0800

    Update BERTOnly.py
    Edit Code Comments

--- models/BERTOnly.py
+++ models/BERTOnly.py
@@ -7,15 +7,15 @@ class BERTOnly(nn.Module):
         super(BERTOnly, self).__init__()
         self.bert = bert
         self.dropout = nn.Dropout(opt.dropout)
-        # 将输入维度从 100 改为 768，以匹配 BERT 的输出
-        self.classifier = nn.Linear(768, opt.polarities_dim)  # opt.polarities_dim 应该是你要分类的类别数，例如 2 表示二分类
+        # Change the input dimension from 100 to 768 to match Bert's output
+        self.classifier = nn.Linear(768, opt.polarities_dim)  # opt.polarities_dim
 
     def forward(self, inputs):
         text_bert_indices, bert_segments_ids = inputs[0], inputs[1]
-        # 使用 BERT 提取特征
+        # Extracting features using Bert
         _, pooled_output = self.bert(text_bert_indices, token_type_ids=bert_segments_ids, output_all_encoded_layers=False)
         pooled_output = self.dropout(pooled_output)
-        # 使用全连接层进行分类
+        # Classification using the fully connected layer
         output = self.classifier(pooled_output)
         return output
 
